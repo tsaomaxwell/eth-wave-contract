e7312754c5004cf6450bd6a993634504f35b1b01
tsaomaxwell <68205375+tsaomaxwell@users.noreply.github.com>
2021-09-13 20:17:24 -0700

    basic eth contract setup

---
+++ contracts/WavePortal.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract WavePortal {
+    uint totalWaves;
+    string lastWaver;
+    constructor() {
+        console.log("if we have a decentralized web, how is mr. spider going to catch bugs");
+    }
+
+    function wave() public {
+        totalWaves += 1;
+        lastWaver = toAsciiString(msg.sender);
+        console.log("%s is waved!", msg.sender);
+    }
+
+    //tkeber on stack exchange
+    function toAsciiString(address x) internal pure returns (string memory) {
+        bytes memory s = new bytes(40);
+        for (uint i = 0; i < 20; i++) {
+            bytes1 b = bytes1(uint8(uint(uint160(x)) / (2**(8*(19 - i)))));
+            bytes1 hi = bytes1(uint8(b) / 16);
+            bytes1 lo = bytes1(uint8(b) - 16 * uint8(hi));
+            s[2*i] = char(hi);
+            s[2*i+1] = char(lo);            
+        }
+        return string(s);
+    }
+    //tkeber on stack exchange
+    function char(bytes1 b) internal pure returns (bytes1 c) {
+        if (uint8(b) < 10) return bytes1(uint8(b) + 0x30);
+        else return bytes1(uint8(b) + 0x57);
+    }
+
+    function getTotalWaves() view public returns (uint) {
+        console.log("We have %d total waves", totalWaves);
+        return totalWaves;
+    }
+
+    function getLastWaver() view public returns (string memory) {
+        console.log("%s waved last", lastWaver);
+        return lastWaver;
+    }
+}
