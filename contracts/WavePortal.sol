ba91b0270ad9e0e85ba26d8328fb94ac8a958f5c
tsaomaxwell <68205375+tsaomaxwell@users.noreply.github.com>
2021-09-13 23:45:59 -0700

    final deployed with random reward

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -7,6 +7,7 @@ import "hardhat/console.sol";
 contract WavePortal {
     uint totalWaves;
     string lastWaver;
+    uint private seed;
 
     event NewWave(address indexed from, uint timestamp, string message);
 
@@ -18,24 +19,35 @@ contract WavePortal {
 
     Wave[] waves;
 
+    mapping(address => uint) public lastWavedAt;
+
     constructor() payable{
         console.log("if we have a decentralized web, how is mr. spider going to catch bugs");
     }
 
     function wave(string memory _message) public {
+        require(lastWavedAt[msg.sender] + 15 minutes < block.timestamp, "Wait 15m");
+        lastWavedAt[msg.sender] = block.timestamp;
+
         totalWaves += 1;
         lastWaver = toAsciiString(msg.sender);
         console.log("%s waved w/ %s!", msg.sender, _message);
 
         waves.push(Wave(msg.sender, _message, block.timestamp));
 
+        uint randomNumber = (block.difficulty + block.timestamp + seed) % 100;
+        console.log("number is", randomNumber);
+        seed = randomNumber;
+
+        if(randomNumber < 50) {
+            console.log("%s is paid", msg.sender);
+            uint prizeAmount = 0.00001 ether;
+            require(prizeAmount <= address(this).balance, "Oops no more conceptual bucks");
+            (bool success,) = (msg.sender).call{value: prizeAmount}("");
+            require(success, "Failed to send te moonies");
+        }
+
         emit NewWave(msg.sender, block.timestamp, _message);
-
-
-        uint prizeAmount = 0.00001 ether;
-        require(prizeAmount <= address(this).balance, "Oops no more conceptual bucks");
-        (bool success,) = (msg.sender).call{value: prizeAmount}("");
-        require(success, "Failed to send te moonies");
     }
 
     //tkeber on stack exchange
